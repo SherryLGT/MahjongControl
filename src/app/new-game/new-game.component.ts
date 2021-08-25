ef45ceafd0c7abf73b7eb2e34b7061b242aa14d2
Sherry <sherry@slgt.io>
2021-08-26 02:03:40 +0800

    Completed selective seat selection and unseat all - constants added DEV | PROD mode

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -7,14 +7,7 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 
 import _ from "lodash";
 import {PLAYER} from "../../interface";
-import {PLAYER_COOKIE} from "../../constants";
-
-// enum Wind {
-//   Dong,
-//   Nan,
-//   Xi,
-//   Bei
-// }
+import {STATE, PLAYER_COOKIE} from "../../constants";
 
 @Component({
   selector: 'new-game-component',
@@ -36,20 +29,22 @@ export class NewGameComponent implements OnInit {
   playersSelectedList = new Set<PLAYER>();
   allSelected: boolean = false;
 
-  // playerAltList: PLAYER[] = [];
-  playerAltList: PLAYER[] = [];
-  playerSeatL = new FormControl();
-  playerSeatR = new FormControl();
+  // Stores player seat selection
   playerSeatT = new FormControl();
   playerSeatB = new FormControl();
+  playerSeatL = new FormControl();
+  playerSeatR = new FormControl();
+  playerSeatListT: PLAYER[] = [];
+  playerSeatListB: PLAYER[] = [];
+  playerSeatListL: PLAYER[] = [];
+  playerSeatListR: PLAYER[] = [];
 
   ngOnInit(): void {
     if (!this.refreshCookie()) {
       this.openEditPlayerDialog();
     }
 
-    // DEVELOPMENT MODE
-    if (this.players.value === null) {
+    if (STATE === "DEV" && this.players.value === null) {
       const temp: PLAYER[] = [];
       temp.push(this.playerList[0]);
       temp.push(this.playerList[1]);
@@ -94,14 +89,114 @@ export class NewGameComponent implements OnInit {
       this.players.setValue(this.playersSelectedList);
     }
     this.allSelected = this.players.value.length == 4;
-    this.playerAltList = _.clone(this.playerList);
+    if (this.allSelected) {
+      this.resetSeat();
+    }
   }
 
-  playerSeatSelected() { // TODO
-    console.log(this.playerAltList);
-    console.log(this.playerSeatL.value);
-    console.log(this.playerSeatR.value);
-    console.log(this.playerSeatT.value);
-    console.log(this.playerSeatB.value);
+  playerSeatSelected(player, pos) {
+    const indexT = this.playerSeatListT.findIndex((p) => p === player);
+    const indexB = this.playerSeatListB.findIndex((p) => p === player);
+    const indexL = this.playerSeatListL.findIndex((p) => p === player);
+    const indexR = this.playerSeatListR.findIndex((p) => p === player);
+
+    switch(pos) {
+      case "TOP" :
+        this.playerSeatListB.splice(indexB, 1);
+        this.playerSeatListL.splice(indexL, 1);
+        this.playerSeatListR.splice(indexR, 1);
+
+        this.syncPlayerSeatList(player, this.playerSeatListT, this.playerSeatListB);
+        this.syncPlayerSeatList(player, this.playerSeatListT, this.playerSeatListL);
+        this.syncPlayerSeatList(player, this.playerSeatListT, this.playerSeatListR);
+        break;
+      case "BOTTOM" :
+        this.playerSeatListT.splice(indexT, 1);
+        this.playerSeatListL.splice(indexL, 1);
+        this.playerSeatListR.splice(indexR, 1);
+
+        this.syncPlayerSeatList(player, this.playerSeatListB, this.playerSeatListT);
+        this.syncPlayerSeatList(player, this.playerSeatListB, this.playerSeatListL);
+        this.syncPlayerSeatList(player, this.playerSeatListB, this.playerSeatListR);
+        break;
+      case "LEFT" :
+        this.playerSeatListT.splice(indexT, 1);
+        this.playerSeatListB.splice(indexB, 1);
+        this.playerSeatListR.splice(indexR, 1);
+
+        this.syncPlayerSeatList(player, this.playerSeatListL, this.playerSeatListT);
+        this.syncPlayerSeatList(player, this.playerSeatListL, this.playerSeatListB);
+        this.syncPlayerSeatList(player, this.playerSeatListL, this.playerSeatListR);
+        break;
+      case "RIGHT" :
+        this.playerSeatListT.splice(indexT, 1);
+        this.playerSeatListB.splice(indexB, 1);
+        this.playerSeatListL.splice(indexL, 1);
+
+        this.syncPlayerSeatList(player, this.playerSeatListR, this.playerSeatListT);
+        this.syncPlayerSeatList(player, this.playerSeatListR, this.playerSeatListB);
+        this.syncPlayerSeatList(player, this.playerSeatListR, this.playerSeatListL);
+    }
+  }
+
+  syncPlayerSeatList(check, main, sub) {
+    const diff = main.find(p => p !== check && !sub.includes(p));
+    if (diff) sub.push(diff);
+  }
+
+  removePlayerSeat(player, pos) {
+    switch(pos) {
+      case "TOP" :
+        this.playerSeatListB.push(player);
+        this.playerSeatListL.push(player);
+        this.playerSeatListR.push(player);
+        this.playerSeatT.setValue(null);
+        break;
+      case "BOTTOM" :
+        this.playerSeatListT.push(player);
+        this.playerSeatListL.push(player);
+        this.playerSeatListR.push(player);
+        this.playerSeatB.setValue(null);
+        break;
+      case "LEFT" :
+        this.playerSeatListT.push(player);
+        this.playerSeatListB.push(player);
+        this.playerSeatListR.push(player);
+        this.playerSeatL.setValue(null);
+        break;
+      case "RIGHT" :
+        this.playerSeatListT.push(player);
+        this.playerSeatListB.push(player);
+        this.playerSeatListL.push(player);
+        this.playerSeatR.setValue(null);
+        break;
+    }
+  }
+
+  randomSeat() {
+    if (this.playerSeatT.value === null && this.playerSeatB.value === null && this.playerSeatL.value === null && this.playerSeatR.value === null ||
+        this.playerSeatT.value && this.playerSeatB.value && this.playerSeatL.value && this.playerSeatR.value) {
+      const list = _.shuffle(_.clone(this.playerList));
+      this.playerSeatT.setValue(list[0]);
+      this.playerSeatListT = [list[0]];
+      this.playerSeatB.setValue(list[1]);
+      this.playerSeatListB = [list[1]];
+      this.playerSeatL.setValue(list[2]);
+      this.playerSeatListL = [list[2]];
+      this.playerSeatR.setValue(list[3]);
+      this.playerSeatListR = [list[3]];
+    } else { // TODO : Random remainder
+    }
+  }
+
+  resetSeat() {
+    this.playerSeatT.setValue(null);
+    this.playerSeatB.setValue(null);
+    this.playerSeatL.setValue(null);
+    this.playerSeatR.setValue(null);
+    this.playerSeatListT = _.clone(this.playerList);
+    this.playerSeatListB = _.clone(this.playerList);
+    this.playerSeatListL = _.clone(this.playerList);
+    this.playerSeatListR = _.clone(this.playerList);
   }
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+export const STATE = "DEV";
+// export const STATE = "PROD";
+
 export const MAX_PLAYER = 4;
 
 export const PLAYER_COOKIE = 'player-list';
